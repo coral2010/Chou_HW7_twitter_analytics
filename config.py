7e34cc483be9a007245abdb511ada5abc58f995b
vchou <victoriajchou@gmail.com>
2018-07-29 16:22:57 -0700

    media organization twitter analysis using VADER

---
+++ config.py
@@ -0,0 +1,8 @@
+# These keys are not valid. Use your own API keys
+
+consumer_key = "vcwyegoaRqRO0ZenQOZYgxn0r"
+consumer_secret = "Krwi85zPjDM6ZzRPfaxOxb7xBn7jLjUld3odUTUhd1rYQy7vue"
+access_token = "1019822197290950656-PIoTVED2GkePUo9qkQ7tBPyk782giE"
+access_token_secret = "kOVfNHHCB3t6G29gmSks7r5r6KP878vo1EKsf8Ip1acCv"
+
+weather_api_key = "0805b56a317164d60b9b77c4374b6ba0"
