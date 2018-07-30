98b783b759b0fb72a80f1148e80c42dc4bc84570
coral2010 <37481496+coral2010@users.noreply.github.com>
2018-07-30 10:49:26 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,3 @@ consumer_key = "vcwyegoaRqRO0ZenQOZYgxn0r"
 consumer_secret = "Krwi85zPjDM6ZzRPfaxOxb7xBn7jLjUld3odUTUhd1rYQy7vue"
 access_token = "1019822197290950656-PIoTVED2GkePUo9qkQ7tBPyk782giE"
 access_token_secret = "kOVfNHHCB3t6G29gmSks7r5r6KP878vo1EKsf8Ip1acCv"
-
-weather_api_key = "0805b56a317164d60b9b77c4374b6ba0"
